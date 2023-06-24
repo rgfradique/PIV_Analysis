22dfdc3148c354816824638bb0ba4b094f718012
RGF <rgfradique@gmail.com>
2023-06-24 13:34:29 +0100

    Changed 2nd stage threshold method, uses Otsu's method to detect active areas based on NT samples

--- post_analysis.m
+++ post_analysis.m
@@ -1,13 +1,3 @@
-%{
-% Version 1.0
-% © Ricardo Fradique,Nicola Pellicciotta  2023 (rgf34@cam.ac.uk) 
-% 
-% post_analysis.m is licensed under a Creative Commons 
-% Attribution-NonCommercial-NoDerivatives 4.0 International License.s
-% 
-% Original work
-%
-%}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% STEP 2------------ post analysis  ------------------ %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -17,23 +7,48 @@
 % circular culture plate ). After save everything in a _post.mat file  
 
 clear all
-fps=10/6; %% Only applicable to the slow condition on our files, should be adjusted case by case
+fps=10/6;
 
-filenames = dir('6percent*.mat');
+% Load all NT files and calculate the threshold for active areas based on
+% Otsu's method
+filenames = dir('*NT*.tif.mat');
+cum_magn_u = [];
+for dd=1:numel(filenames)
+    filename=filenames(dd).name;
+    load(filename)
+    u1m=mean(u1,3);
+    v1m=mean(v1,3);
+
+    Mu = sqrt(u1m.^2 +v1m.^2);
+    Mu_l = reshape(Mu,1,[]);
+    cum_magn_u = [cum_magn_u Mu_l];
+end
+h = histogram(cum_magn_u);
+cutout = otsuthresh(h.Values)
+
+% Clear the cumulative vectors
+cum_magn_u = [];
+filtered_magn = [];
+% Load all files sequentially and run threshold based on previous value
+filenames = dir('*.tif.mat');
 
 for dd=1:numel(filenames)
     filename=filenames(dd).name;
     load(filename)
     u1m=mean(u1,3);
     v1m=mean(v1,3);
+
+    Mu = sqrt(u1m.^2 +v1m.^2);
     
     u1mean=mean(u1,3);
     v1mean=mean(v1,3);
     
     u1norm = abs(u1m)./max(abs(u1m),[],'all');
     v1norm = abs(v1m)./max(abs(v1m),[],'all');
-    cutout = 0.3;
-    cutout_matrix = (u1norm > cutout | v1norm > cutout);
+    cutout_matrix = Mu > cutout;
+    Mu_l = reshape(Mu,1,[]);
+    cum_magn_u = [cum_magn_u Mu_l];
+    filtered_magn = [filtered_magn reshape(Mu(cutout_matrix),1,[])];
 
     subplot(2,1,1)
     quiver(x(cutout_matrix),y(cutout_matrix),u1m(cutout_matrix),v1m(cutout_matrix),'b')
@@ -46,15 +61,14 @@ for dd=1:numel(filenames)
     u1m=u1m(cutout_matrix);
     v1m=v1m(cutout_matrix);
     
-    %% Select a circle in the center of the FOV, slightly smaller than the insert diameter
     r = sqrt(diff(px).^2+diff(py).^2) ;
     th = linspace(0,2*pi) ;
     xc = px(1)+r*cos(th) ; 
     yc = py(1)+r*sin(th) ; 
     plot(xc(1,:),yc(1,:),'b.') ;
     % Keep only points lying inside circle
-    perc_mov_area = (size(u1m,1) * size(u1m,2)) / (size(u1mean,1) * size(u1mean,2));
     idx = inpolygon(x(cutout_matrix),y(cutout_matrix),xc(1,:)',yc(1,:)) ;
+    perc_mov_area = (size(u1m,1) * size(u1m,2)) / (size(u1mean,1) * size(u1mean,2));
     
     if sum(cutout_matrix(idx)) == 0
         v1m = v1mean;
@@ -67,9 +81,6 @@ for dd=1:numel(filenames)
     
     % normalise vector for the orientation    
     M = sqrt(u1m.^2 +v1m.^2);
-    
-
-    Mm=nanmedian(M(:));
     
     nu= u1m./M; 
     nv= v1m./M;
@@ -84,5 +95,6 @@ for dd=1:numel(filenames)
     set(gcf,'position',[x0,y0,width,height]);set(gca,'FontSize',15);
     saveas(gcf,strcat(filename(1:end-4),'_PIV_result.pdf'));
     close all
+
     save(strcat(filename(1:end-4),'_post.mat'))
 end
